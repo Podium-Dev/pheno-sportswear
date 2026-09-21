e4336e552b77efec1bf58fa7e736a04620e1db22
romeo516-cmd <romeo@podiumapps.com>
2026-09-21 14:04:15 +0800

    Limit homepage hero slider to three images

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -91,7 +91,6 @@ const retailHeroSlides = [
   { src: "/images/type-1-hoodie-transparent.png", alt: "Type 1 Hoodie, PHENO Sportswear", isCutout: true },
   { src: "/images/type-1-joggers-transparent.png", alt: "Type 1 Joggers, PHENO Sportswear", isCutout: true },
   { src: "/images/type-1-shorts-hero-cutout.png", alt: "Type 1 Shorts, PHENO Sportswear", isCutout: true },
-  { src: "/images/type-1-tank-black-transparent.png", alt: "Type 1 Tank, PHENO Sportswear", isCutout: true },
 ];
 
 function RetailHero() {
